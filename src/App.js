924a633f9f1e90237ede2ac45435ac6b3e774a80
Steven Woodward <steveanthony999@gmail.com>
2022-01-22 23:06:08 -0800

    set up loading screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useEffect, useState } from 'react';
+import LoadingScreen from 'react-loading-screen';
+
+import Logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false);
+    }, 5000);
+  }, []);
+
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <LoadingScreen
+      loading={isLoaded}
+      bgColor='#f1f1f1'
+      spinnerColor='#000000'
+      // textColor='#676767'
+      // logoSrc={Logo}
+      // text='Here an introduction sentence (Optional)'
+    >
+      <div className='App'>
+        <h1>Hello World</h1>
+      </div>
+    </LoadingScreen>
   );
 }
 
